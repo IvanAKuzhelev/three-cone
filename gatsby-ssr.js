c48bc904176aece37788fb03e39edf1117cb1fbd
IvanAKuzhelelev <kuzhelevdesign@gmail.com>
2021-05-08 02:32:42 +0300

    -ssr

--- gatsby-ssr.js
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import RootWrapper from "./src/components/RootWrapper";
-
-const wrapRootElement = ({ element }) => <RootWrapper>{element}</RootWrapper>;
-
-export { wrapRootElement };
